ddb8abc57273b29ca1aeb4c99e17850cc2d1583e
Sérgio Batista <mail@se.rg.io>
2016-08-06 11:39:53 +0200

    version 0.0.2

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,14 @@
-'use strict';
+(function () {
 
-console.log('\'Allo \'Allo! Content script');
+  'use strict';
+
+  if (window.location.host.indexOf('localhost') > -1 ||
+      window.location.host.indexOf('127.0.0.1') > -1) {
+    var text = 'localhost';
+
+    var ribbon =
+      '<div id="where-am-i-ribbon">' + text + '</div>';
+
+    document.body.insertAdjacentHTML('beforeend', ribbon);
+  }
+})();
